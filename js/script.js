68f9556f4cc62cdaa98281f4de7a644fc89981a3
SimoneDiPrima <simonediprimagenova@gmail.com>
2022-06-27 15:22:51 +0200

    creazione variabile per racchiudere array immagini

--- js/script.js
+++ js/script.js
@@ -1,1 +1,4 @@
-console.log(`js ok`);
+
+// creazione array ed eliminazione conseguente immagini lato html
+const allImages = ["./img/01.jpg","./img/02.jpg","./img/03.jpg","./img/04.jpg","./img/05.jpg","./img/06.jpg","./img/07.jpg","./img/08.jpg","./img/09.jpg","./img/10.jpg"];
+
