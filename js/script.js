3247a6d7fe4f8b19679cf38334d5963dec7a4ba3
SimoneDiPrima <simonediprimagenova@gmail.com>
2022-06-27 17:01:23 +0200

    creazione variabile confronto array e bottone

--- js/script.js
+++ js/script.js
@@ -1,10 +1,21 @@
 
 // creazione array ed eliminazione conseguente immagini lato html
-const allImages = ["./img/01.jpg","./img/02.jpg","./img/03.jpg","./img/04.jpg","./img/05.jpg","./img/06.jpg","./img/07.jpg","./img/08.jpg","./img/09.jpg","./img/10.jpg"];
+let allImages = ["./img/01.jpg","./img/02.jpg","./img/03.jpg","./img/04.jpg","./img/05.jpg","./img/06.jpg","./img/07.jpg","./img/08.jpg","./img/09.jpg","./img/10.jpg"];
 console.log(allImages);
-let currentImageindex = 0;
-console.log(currentImageindex);
+let currentImageindex = document.getElementsByClassName("gallery");
 
-allImages[currentImageindex].classList.add(`active`);
+
+const rightButton = document.getElementById(`rightarrow`);
+const leftButton = document.getElementById(`leftarrow`);
+
+rightButton.addEventListener(`click`, function(){
+    for(let i = 0 ; i <= allImages.length; i++){
+        currentImageindex++;
+        allImages[currentImageindex].classList.add(`active`);
+        console.log(currentImageindex[i]);
+    }
+})
+allImages.innerHTML = currentImageindex;
+
 
 
