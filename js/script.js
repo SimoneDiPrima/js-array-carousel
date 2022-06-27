b7f057172ae07412d8e03ba1e0d8be6b99ee2aba
SimoneDiPrima <simonediprimagenova@gmail.com>
2022-06-27 22:41:02 +0200

    da rivedere logica

--- js/script.js
+++ js/script.js
@@ -1,21 +1,23 @@
 
-// creazione array ed eliminazione conseguente immagini lato html
-let allImages = ["./img/01.jpg","./img/02.jpg","./img/03.jpg","./img/04.jpg","./img/05.jpg","./img/06.jpg","./img/07.jpg","./img/08.jpg","./img/09.jpg","./img/10.jpg"];
-console.log(allImages);
-let currentImageindex = document.getElementsByClassName("gallery");
+// // creazione array ed eliminazione conseguente immagini lato html
+ const allImages = ["img/01.jpg","img/02.jpg","img/03.jpg","img/04.jpg","img/05.jpg"];
+ let currentIndeximages = 0;
+ const gallery = document.getElementById(`gallery`);
+ gallery.innerText = allImages[currentIndeximages[0]];
+ let elements = ``;
+ for(let i = 0; i <allImages.length;i++){
+    elements += `<img>${allImages[i]}</img>`;
+ }
+ gallery.innerText = elements;
+let images = document.getElementById('gallery').classList.add('active');
 
+nextButton = document.getElementById(`rightarrow`);
 
-const rightButton = document.getElementById(`rightarrow`);
-const leftButton = document.getElementById(`leftarrow`);
+beforeButton = document.getElementById(`leftarrow`);
 
-rightButton.addEventListener(`click`, function(){
-    for(let i = 0 ; i <= allImages.length; i++){
-        currentImageindex++;
-        allImages[currentImageindex].classList.add(`active`);
-        console.log(currentImageindex[i]);
-    }
+nextButton.addEventListener(`click`, function(){
+    images[currentIndeximages].classList.remove(`active`);
+    currentIndeximages++ ;
+    images[currentIndeximages].classList.add(`active`);
 })
-allImages.innerHTML = currentImageindex;
-
-
 
