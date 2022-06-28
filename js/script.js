b009588098810e00078abfadde036a3a9d893d73
SimoneDiPrima <simonediprimagenova@gmail.com>
2022-06-28 16:50:33 +0200

    esercizio completo da aggiungere thumbnails

--- js/script.js
+++ js/script.js
@@ -1,27 +1,43 @@
-// creo il mio array e prendo la variabile "contenitore";la variabile d appoggio per l indice dell immagine;
+// prendo la classe gallery grazie alla variabile sotto e al querySelector.
 const gallery = document.querySelector(`#carousel .gallery`);
+// creo il mio array con tutte le immagini dentro
 const sources =[`img/01.jpg`,`img/02.jpg`,`img/03.jpg`,`img/04.jpg`,`img/05.jpg`,`img/06.jpg`,`img/07.jpg`,`img/08.jpg`,`img/09.jpg`,`img/10.jpg`]
 
-
+// creo la mia variabile vuota sul quale costruire il mio `stringone(con template literal)`
 let imageElements = ``;
 for(let i = 0; i <sources.length;i++){
+    // monto tutti i pezzi singoli finche non avro preso tutti gli elementi del mio array
     imageElements += `<img src="${sources[i]}" alt="foto paesaggio"/>`; 
  }
  gallery.innerHTML = imageElements;
-
+// prendo tutte le immagini grazie al querySelectorAll
 const images = document.querySelectorAll(`#carousel img`); //mi restituisce una lista di nodi
-
-let allIndeximages = 0;
-
+// creo l indice d appoggio 
+let allIndeximages = 0; 
+// metto la classe active alla prima immagine in modo da vedere solo quella
 images[allIndeximages].classList.add(`active`);
 
-
+// creo le due variabili bottoni al quale leghero il cambio delle immagini e al quale devo mettere il controllo affinche ripartano
+// dall inizio quando arrivano all ultima immagine
 nextButton = document.getElementById(`rightarrow`);
 
 beforeButton = document.getElementById(`leftarrow`);
 
 nextButton.addEventListener(`click`, function(){
-    images[currentIndeximages].classList.remove(`active`);
-    currentIndeximages++ ;
-    images[currentIndeximages].classList.add(`active`);
+    
+    images[allIndeximages].classList.remove(`active`);
+    allIndeximages++ ;
+    if(allIndeximages===images.length){
+        allIndeximages = 0;
+    }
+    images[allIndeximages].classList.add(`active`);
 })
+beforeButton.addEventListener(`click`, function(){
+    images[allIndeximages].classList.remove(`active`);
+    allIndeximages-- ;
+    if(allIndeximages < 0){
+        allIndeximages = (images.length - 1);
+    }
+    images[allIndeximages].classList.add(`active`);
+})
+
