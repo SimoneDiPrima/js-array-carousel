a9624bde71c60be01cd55782c3d8c1f46c46e314
SimoneDiPrima <simonediprimagenova@gmail.com>
2022-06-28 15:36:26 +0200

    modifiche logica da vedere gestione bottoni

--- js/script.js
+++ js/script.js
@@ -1,15 +1,20 @@
+// creo il mio array e prendo la variabile "contenitore";la variabile d appoggio per l indice dell immagine;
+const gallery = document.querySelector(`#carousel .gallery`);
+const sources =[`img/01.jpg`,`img/02.jpg`,`img/03.jpg`,`img/04.jpg`,`img/05.jpg`,`img/06.jpg`,`img/07.jpg`,`img/08.jpg`,`img/09.jpg`,`img/10.jpg`]
 
-// // creazione array ed eliminazione conseguente immagini lato html
- const allImages = ["img/01.jpg","img/02.jpg","img/03.jpg","img/04.jpg","img/05.jpg"];
- let currentIndeximages = 0;
- const gallery = document.getElementById(`gallery`);
- gallery.innerText = allImages[currentIndeximages[0]];
- let elements = ``;
- for(let i = 0; i <allImages.length;i++){
-    elements += `<img>${allImages[i]}</img>`;
+
+let imageElements = ``;
+for(let i = 0; i <sources.length;i++){
+    imageElements += `<img src="${sources[i]}" alt="foto paesaggio"/>`; 
  }
- gallery.innerText = elements;
-let images = document.getElementById('gallery').classList.add('active');
+ gallery.innerHTML = imageElements;
+
+const images = document.querySelectorAll(`#carousel img`); //mi restituisce una lista di nodi
+
+let allIndeximages = 0;
+
+images[allIndeximages].classList.add(`active`);
+
 
 nextButton = document.getElementById(`rightarrow`);
 
@@ -20,4 +25,3 @@ nextButton.addEventListener(`click`, function(){
     currentIndeximages++ ;
     images[currentIndeximages].classList.add(`active`);
 })
-
